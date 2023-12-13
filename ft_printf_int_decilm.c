791efd58acde3b8d8f2bf65ebd3d61c4ee1c601e
pokpalae <pokpalae@student.42wolfsburg.de>
2023-12-13 13:36:34 +0100

    minor changes

--- ft_printf_int_decilm.c
+++ ft_printf_int_decilm.c
@@ -6,13 +6,11 @@
 /*   By: pokpalae <pokpalae@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/12 16:28:40 by pokpalae          #+#    #+#             */
-/*   Updated: 2023/12/12 16:42:03 by pokpalae         ###   ########.fr       */
+/*   Updated: 2023/12/13 13:35:57 by pokpalae         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-
-int	ft_putnbr(int n);
 
 int	ft_putnbr(int n)
 {
